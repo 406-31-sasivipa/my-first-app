b04dae992dbed75255125921490c99a6907501df
406-31-sasivipa <54662@yupparaj.ac.th>
2026-08-21 11:04:36 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -68,7 +68,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
 if score == 4: st.success("🎉 You win!") 
-    else: st.error("💀 You lose!")
+else: st.error("💀 You lose!")
 
 
 # ----------------------------------------------------
