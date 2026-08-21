37509b99a7a1df793e4f856412a6815e1078b4e9
406-31-sasivipa <54662@yupparaj.ac.th>
2026-08-21 10:59:52 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -51,8 +51,6 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
-    # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
-
     # ตรวจข้อ 3
     if u_ans3 == "banana":
         st.success("✅ ข้อ 3: ถูกต้อง")
@@ -61,16 +59,15 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4
-    if u_ans2 == "pineapple":
+    if u_ans4 == "pineapple":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
 
-
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-  if score == 2:
+    if score == 4:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
